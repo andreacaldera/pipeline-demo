7009399b58cb7a8bdef6ad5bbd7ebc370a2aa4fc
Andrea Caldera <andrea.caldera@gmail.com>
2023-04-04 08:44:01 +0200

    flaky test

---
+++ test/flaky-test.spec.js
@@ -0,0 +1,14 @@
+jest.setTimeout(10000);
+
+describe("flaky test", () => {
+  it("fails about 30% of the time", async () => {
+    const random = Math.floor(Math.random() * 10);
+
+    expect(random).toBeGreaterThan(3);
+  });
+  it("fails about 50% of the time", async () => {
+    const random = Math.floor(Math.random() * 10);
+
+    expect(random).toBeGreaterThan(6);
+  });
+});
