c962f8b43e7c9ebad6f8bbc868f7c2d5a7a4dcc1
Andrea Caldera <andrea.caldera@gmail.com>
2023-04-04 09:07:32 +0200

    skipped test

--- test/flaky-test.spec.js
+++ test/flaky-test.spec.js
@@ -1,5 +1,3 @@
-jest.setTimeout(10000);
-
 describe("flaky test", () => {
   it("fails about 30% of the time", async () => {
     const random = Math.floor(Math.random() * 10);
---
+++ test/skipped-test.spec.js
@@ -0,0 +1,5 @@
+describe.skip("skipped test", () => {
+  it("does not run", async () => {
+    expect(true).toBeTruthy();
+  });
+});
